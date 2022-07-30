f4385705bff3be31eccb67a5c38eb98cbd365633
Chaiko113 <108830634+Chaiko113@users.noreply.github.com>
2022-07-30 14:11:13 +0200

    Sistemato problema blocco laterale piattaforme attraversabili

--- Assets/Scripts/Enviroment/TraversablePlatform.cs
+++ Assets/Scripts/Enviroment/TraversablePlatform.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class TraversablePlatform : MonoBehaviour {
 
@@ -9,19 +10,67 @@ public class TraversablePlatform : MonoBehaviour {
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    //private void OnCollisionEnter2D(Collision2D other)
+    //{
+    //    Debug.Log("1 " + this.gameObject.GetComponent<BoxCollider2D>().name);
+    //    Debug.Log("2 " + other.gameObject.name);
+    //    switch (other.gameObject.tag)
+    //    {
+    //        case "Player":
+    //            if (other.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0)
+    //            {
+    //                boxCollider.enabled = false;
+    //            }
+    //            else
+    //                boxCollider.enabled = true;
+    //            break;
+    //        case "BouncingTip":
+    //            Debug.Log(other.gameObject.transform.parent.gameObject.name);
+    //            if (other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0)
+    //            {
+    //                boxCollider.enabled = false;
+    //            }
+    //            else
+    //                boxCollider.enabled = true;
+    //            break;
+    //    }
+    //}
 
-        switch (other.gameObject.tag) {
+    public void ClearLog()
+    {
+        var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+        var type = assembly.GetType("UnityEditor.LogEntries");
+        var method = type.GetMethod("Clear");
+        method.Invoke(new object(), null);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ClearLog();
+        switch (other.gameObject.tag)
+        {
             case "Player":
-                if (other.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0) {
+                Vector2 vectorP = new Vector2(other.gameObject.GetComponent<Rigidbody2D>().transform.position.x, other.gameObject.GetComponent<Rigidbody2D>().transform.position.y);
+                if (other.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0 ||
+                    vectorP.x <= (this.gameObject.GetComponent<BoxCollider2D>().transform.position.x - (this.gameObject.GetComponent<BoxCollider2D>().transform.localScale.x / 2)) ||
+                    vectorP.x >= this.gameObject.GetComponent<BoxCollider2D>().transform.position.x + (this.gameObject.GetComponent<BoxCollider2D>().transform.localScale.x / 2))
+                {
                     boxCollider.enabled = false;
                 }
                 else
+                {
+                    Debug.Log(other.gameObject.GetComponent<Rigidbody2D>().velocity.y);
                     boxCollider.enabled = true;
+                }
                 break;
             case "BouncingTip":
-                Debug.Log(other.gameObject.transform.parent.gameObject.name);
-                if (other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0) {
+                Debug.Log(other.gameObject.name); 
+                Vector2 vectorB = new Vector2(other.gameObject.GetComponent<CapsuleCollider2D>().transform.position.x, other.gameObject.GetComponent<CapsuleCollider2D>().transform.position.y);
+
+                if (other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>().velocity.y > 0 ||
+                    vectorB.x <= (this.gameObject.GetComponent<BoxCollider2D>().transform.position.x - (this.gameObject.GetComponent<BoxCollider2D>().transform.localScale.x / 2)) ||
+                    vectorB.x >= this.gameObject.GetComponent<BoxCollider2D>().transform.position.x + (this.gameObject.GetComponent<BoxCollider2D>().transform.localScale.x / 2))
+                {
                     boxCollider.enabled = false;
                 }
                 else
